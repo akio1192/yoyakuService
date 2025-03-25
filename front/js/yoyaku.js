8f2ad8d14a1523f95affe4c43fcca50ee212cc93
藤本晃央 <fujimoto_akio@strikesystem.co.jp>
2025-03-25 18:38:55 +0900

    予約希望日①、②、③を追加

--- front/js/yoyaku.js
+++ front/js/yoyaku.js
@@ -8,10 +8,10 @@ function init() {
   let yoyakuDatetime = sessionStorage.getItem("yoyakuDatetime");
   let yoyakuNinzu = sessionStorage.getItem("yoyakuNinzu");
   let yoyakuTenMei = sessionStorage.getItem("yoyakuTenMei");
-  let date = document.getElementById("date");
+  let date1 = document.getElementById("date1");
   let people = document.getElementById("people");
   let tenMei = document.getElementById("tenMei");
-  date.value = yoyakuDatetime;
+  date1.value = yoyakuDatetime;
   people.value =
     yoyakuNinzu != null ? yoyakuNinzu.replace(/[^0-9]/g, "") : yoyakuNinzu;
   tenMei.value = yoyakuTenMei;
@@ -54,7 +54,12 @@ document.addEventListener("DOMContentLoaded", function () {
     storePlaceholder: document.getElementById("tenMei"),
     contact: document.querySelector("label[for='contact']"),
     contactPlaceholder: document.getElementById("contact"),
-    date: document.querySelector("label[for='date']"),
+    date1: document.querySelector("label[for='date1']"),
+    date2: document.querySelector("label[for='date2']"),
+    date3: document.querySelector("label[for='date3']"),
+    date1Placeholder: document.getElementById("date1"),
+    date2Placeholder: document.getElementById("date2"),
+    date3Placeholder: document.getElementById("date3"),
     address: document.querySelector("label[for='address']"),
     addressPlaceholder: document.getElementById("address"),
     notes: document.querySelector("label[for='notes']"),
@@ -77,7 +82,12 @@ document.addEventListener("DOMContentLoaded", function () {
       storePlaceholder: "予約店舗名",
       contact: "連絡先",
       contactPlaceholder: "自身の電話番号",
-      date: "予約希望日",
+      date1: "予約希望日①",
+      date2: "予約希望日②",
+      date3: "予約希望日③",
+      date1Placeholder: "予約希望日①",
+      date2Placeholder: "予約希望日②",
+      date3Placeholder: "予約希望日③",
       address: "住所（任意）",
       addressPlaceholder: "住所",
       notes: "特記事項（任意）",
@@ -97,7 +107,12 @@ document.addEventListener("DOMContentLoaded", function () {
       storePlaceholder: "Restaurant Name",
       contact: "Contact Number",
       contactPlaceholder: "Your phone number",
-      date: "Preferred Date",
+      date1: "Preferred Date①",
+      date2: "Preferred Date②",
+      date3: "Preferred Date③",
+      date1Placeholder: "Preferred Date①",
+      date2Placeholder: "Preferred Date②",
+      date3Placeholder: "Preferred Date③",
       address: "Address (Optional)",
       addressPlaceholder: "Address",
       notes: "Special Notes (Optional)",
@@ -122,7 +137,12 @@ document.addEventListener("DOMContentLoaded", function () {
       storePlaceholder: "Nom du restaurant",
       contact: "Numéro de contact",
       contactPlaceholder: "Votre numéro de téléphone",
-      date: "Date préférée",
+      date1: "Date préférée①",
+      date2: "Date préférée②",
+      date3: "Date préférée③",
+      date1Placeholder: "Date préférée①",
+      date2Placeholder: "Date préférée②",
+      date3Placeholder: "Date préférée③",
       address: "Adresse (facultatif)",
       addressPlaceholder: "Adresse",
       notes: "Remarques spéciales (facultatif)",
@@ -169,7 +189,21 @@ document.addEventListener("DOMContentLoaded", function () {
         "placeholder",
         translations[langCode].contactPlaceholder
       );
-      textElements.date.textContent = translations[langCode].date;
+      textElements.date1.textContent = translations[langCode].date1;
+      textElements.date2.textContent = translations[langCode].date2;
+      textElements.date3.textContent = translations[langCode].date3;
+      textElements.date1Placeholder.setAttribute(
+        "placeholder",
+        translations[langCode].date1Placeholder
+      );
+      textElements.date2Placeholder.setAttribute(
+        "placeholder",
+        translations[langCode].date2Placeholder
+      );
+      textElements.date3Placeholder.setAttribute(
+        "placeholder",
+        translations[langCode].date3Placeholder
+      );
       textElements.address.textContent = translations[langCode].address;
       textElements.addressPlaceholder.setAttribute(
         "placeholder",
